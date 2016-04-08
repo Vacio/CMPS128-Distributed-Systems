e2f93d7d410b9c3f7b374a25b3a9b3953b3548e1
sborland <sborland@ucsc.edu>
2016-04-07 20:18:01 -0700

    Switch to Python

---
+++ hw1/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Please head to /hello or /echobot'
+
+@app.route("/hello",methods=['GET'])
+def hello():
+    if request.method== 'GET':
+       return "Hello world!"
+
+@app.route("/echo",methods=['GET'])
+def echo():
+    if request.method == 'GET':
+        message = request.args.get('msg')
+        return message  
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=8080)
+    app.debug = True
