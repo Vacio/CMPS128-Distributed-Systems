bf2bd4781a3d48f4e212480b662facdf35fcd925
KatzeWire <jscalett@ucsc.edu>
2016-04-23 13:04:59 -0700

    Added Delete

--- hw2/asg2.py
+++ hw2/asg2.py
@@ -1,29 +1,57 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__)
 app.debug = True
+DT = {}
 
 @app.route("/kvs/<string:key_name>", methods=['GET', 'PUT', 'DELETE'])
 
 def root(key_name):
 	if (request.method == 'GET'):
-		return "Get value of %s" % key_name
+		return getValue(key_name)
 
 	if (request.method == 'PUT'):
-		v = request.form['value']
-		return "Stored %s at %s" % (v, key_name)
+		v = request.form['val']
+		return putValue(key_name,v)
 
 	if (request.method == 'DELETE'):
-		return "Delete value and key: %s" % key_name
-		
+		#return "Delete value and key: %s" % key_name
+		return delValue(key_name)
+
 	else:
 		return "Invalid request."
 
-# Value GET (Key k) Get key-value pair using key
-# PUT(Key k, Value v) Store key-value pair
-# DEL(Key k) Remove key-value pair using key
+# Working
+def getValue(key):
+	if (key in DT):
+		value = DT[key]
+		j = jsonify(msg='success',value=value)
+		return make_response(j,200,{'Content-Type':'application/json'})
+	else:
+		j = jsonify(msg='error',error='key does not exist')
+		return make_response(j,404,{'Content-Type':'application/json'})
 
-# Responses for the GET, PUT, DEL methods
+
+# Working
+def putValue(key,value):
+	if (key in DT):
+		DT[key]=value
+		j = jsonify(msg='success',replaced=1)
+		return make_response(j,200, {'Content-Type' : 'application/json'})
+	else:
+		DT[key]=value
+		j = jsonify(msg='success',replaced=0)
+		return make_response(j,201,{'Content-Type' : 'application/json'})
+		
+# idk
+def delValue(key):
+	if (key in DT):
+		DT.pop(key,None)
+		j = jsonify(msg='success')
+		return make_response(j,200,{'Content-Type':'application/json'})
+	else:
+		j = jsonify(msg='error',error='key does not exist')
+		return make_response(j,201,{'Content-Type':'application/json'})
 
 if __name__ == '__main__':
 	app.run(port=8080)
